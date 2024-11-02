96fcc5fa234a28e97f07373a325f36254cda6d55
lovemilk <zhuhansan666@outlook.com>
2024-11-02 15:55:39 +0800

    add: support multiple handlers for one same exception add: LICENSE

--- module_name/handles/exception_handles.py
+++ module_name/handles/exception_handles.py
@@ -1,6 +1,4 @@
 from asyncio import iscoroutine
-from http.client import responses
-
 from fastapi import FastAPI, Request
 from fastapi.responses import Response
 from fastapi.exceptions import StarletteHTTPException
@@ -8,17 +6,22 @@ from typing import Any, Callable, TypeVar, Coroutine, Awaitable, TypeAlias
 
 from ..structs.responses import BaseResponse
 
+__all__ = (
+    'add_http_exception_handler',
+    'add_exception_handler_middleware'
+)
+
 _ExceptionType = TypeVar("_ExceptionType", bound=type[Exception])
 _ExceptionHandlerType: TypeAlias = Callable[
     [Request, _ExceptionType],
     None | BaseResponse | Response | Awaitable[BaseResponse | Response] | Coroutine[Any, Any, BaseResponse | Response]
 ]
 _exception_handlers: dict[
-    _ExceptionType, _ExceptionHandlerType
+    _ExceptionType, list[_ExceptionHandlerType]
 ] = {}
 
 
-def _get_handler(exc: _ExceptionType) -> _ExceptionHandlerType | None:
+def _get_handler(exc: _ExceptionType) -> list[_ExceptionHandlerType] | None:
     for exc_type, handler in _exception_handlers.items():
         if issubclass(exc, exc_type):  # type: ignore
             return handler
@@ -28,24 +31,28 @@ async def _exception_handler_middleware(request: Request, call_next):
     try:
         return await call_next(request)
     except tuple(_exception_handlers.keys()) as exc:
-        handler = _get_handler(type(exc))
-        if not handler:
+        handlers = _get_handler(type(exc))
+        if not handlers:
             raise
 
-        result = handler(request, exc)
-        if iscoroutine(result):
-            result = await result
+        for handler in handlers:
+            result = handler(request, exc)
+            if iscoroutine(result):
+                result = await result
 
-        if isinstance(result, BaseResponse):
-            return result.to_response()
-        elif isinstance(result, Request):
-            return result
+            if isinstance(result, BaseResponse):
+                return result.to_response()
+            elif isinstance(result, Request):
+                return result
 
         raise
 
 
 def _add_handler(exc: _ExceptionType, handler: _ExceptionHandlerType):
-    _exception_handlers[exc] = handler
+    if exc not in _exception_handlers:
+        _exception_handlers[exc] = []
+
+    _exception_handlers[exc].append(handler)
 
 
 def add_http_exception_handler():
